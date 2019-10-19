a3347c722dfe341c22e03b01b805afa45db5e246
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2019-10-19 01:00:25 -0400

    adding table view as the first page

--- CustomItunes/CustomItunes/MyTableViewController.swift
+++ CustomItunes/CustomItunes/MyTableViewController.swift
@@ -8,12 +8,49 @@
 
 import UIKit
 
-class MyTableViewController: UITableViewController {
+class MyTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var myTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    var cellId = "cellId"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableView()
     }
+    
+    
+    func setupTableView(){
+        myTableView.frame = view.frame
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        view.addSubview(myTableView)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: cellId)
+       
+        
+        cell?.translatesAutoresizingMaskIntoConstraints = false
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
+
 
 
 }
