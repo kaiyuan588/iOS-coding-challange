7688b24c179c936a87dbc540e620cdbd124b06da
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2019-10-18 23:28:51 -0400

    initialize the project

---
+++ CustomItunes/CustomItunes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomItunes
+//
+//  Created by 低调 on 10/18/19.
+//  Copyright © 2019 Kaiyuan Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
