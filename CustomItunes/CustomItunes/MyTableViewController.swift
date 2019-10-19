cdaecef126d39e411ad772e1fc8b67b348dc7444
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2019-10-19 00:27:50 -0400

    initialize project with tableview xcode 11

---
+++ CustomItunes/CustomItunes/MyTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomItunes
+//
+//  Created by 低调 on 10/19/19.
+//  Copyright © 2019 Kaiyuan Zhao. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
